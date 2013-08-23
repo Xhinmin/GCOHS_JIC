c621e6face573cb41312984c9cd1572d536b6a0c
ShinMin <joey514111@hotmail.com>
2013-08-24 00:38:40 +0800

    選取物件與移動 漸變顏色還沒完成

--- GCOHS_JIC/Assets/05_Script/PickObject.cs
+++ GCOHS_JIC/Assets/05_Script/PickObject.cs
@@ -4,6 +4,7 @@ using System.Collections;
 /// <summary>
 /// OHS專案第一步驟：選取圖片
 /// </summary>
+/// 
 public class PickObject : MonoBehaviour
 {
     public GameObject Target;
@@ -14,6 +15,10 @@ public class PickObject : MonoBehaviour
 
     private RaycastHit hit;
 
+    public enum MouseType { 點擊, 拖曳中, 放開 };
+    public MouseType mouseType;
+
+    private float clickTime;
     // Use this for initialization
     void Start()
     {
@@ -25,15 +30,70 @@ public class PickObject : MonoBehaviour
     {
         if (Physics.Raycast(this.camera.ScreenToWorldPoint(Input.mousePosition), new Vector3(0, 0, 1), out this.hit, 100, this.TargetLayer) == true)
         {
-            print(this.hit.transform.name);
-            this.Target = this.hit.transform.gameObject;
+            ProcessMouseState();
         }
         else
             this.Target = null;
+
     }
 
     void OnDrawGizmos()
     {
         Gizmos.DrawRay(this.camera.ScreenToWorldPoint(Input.mousePosition), new Vector3(0, 0, 1) * 100);
+    }
+
+
+    /// <summary>
+    /// 處理滑鼠狀態
+    /// </summary>
+    private void ProcessMouseState()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            mouseType = MouseType.點擊;
+        }
+        else
+        {
+            clickTime = 0;
+            mouseType = MouseType.放開;
+        }
+
+
+        if (mouseType == MouseType.點擊)
+        {
+            iTween.ValueTo(this.Target.gameObject, iTween.Hash(
+"from", 0,
+"to", 255,
+"time", 1,
+"onupdate", "changeMotionBlur"
+));
+
+            clickTime += Time.deltaTime;
+            if (clickTime > 0.5F)
+            {
+                mouseType = MouseType.拖曳中;
+                if (Target) print(Target.transform.name + " 已被選取");
+            }
+        }
+
+        if (mouseType == MouseType.拖曳中)
+        {
+            if (Target)
+                Target.transform.position = new Vector3(this.camera.ScreenToWorldPoint(Input.mousePosition).x,
+                    this.camera.ScreenToWorldPoint(Input.mousePosition).y, Target.transform.position.z);
+        }
+
+        if (mouseType == MouseType.放開)
+        {
+            this.Target = this.hit.transform.gameObject;
+
+
+        }
+
+    }
+
+    void changeMotionBlur(float newValue)
+    {
+        Target.GetComponent<SmoothMoves.Sprite>().color.a = newValue;
     }
 }
