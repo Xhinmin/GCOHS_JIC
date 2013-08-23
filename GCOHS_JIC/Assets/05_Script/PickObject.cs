043cc358995f4cb027732ee3c09e50614ceb288a
ShinMin <joey514111@hotmail.com>
2013-08-24 01:52:33 +0800

    漸變 / 選取 / 回復

--- GCOHS_JIC/Assets/05_Script/PickObject.cs
+++ GCOHS_JIC/Assets/05_Script/PickObject.cs
@@ -15,16 +15,17 @@ public class PickObject : MonoBehaviour
 
     private RaycastHit hit;
 
+    //滑鼠狀態
     public enum MouseType { 點擊, 拖曳中, 放開 };
     public MouseType mouseType;
-
+    //按下累積時間
     private float clickTime;
-
+    //按下累積時間門檻
     public float clickTime_threshold;
     // Use this for initialization
     void Start()
     {
-        mouseType = MouseType.放開;
+        
     }
 
     // Update is called once per frame
@@ -35,8 +36,13 @@ public class PickObject : MonoBehaviour
             ProcessMouseState();
         }
         else
+        {
+            //當超出範圍時(例如滑鼠移動太快，圖片沒跟到的時候) 要把圖片ALPHA調回１
+            mouseType = MouseType.放開;
+            iTween.StopByName("PickObject");
+            if (Target) Target.GetComponent<SmoothMoves.Sprite>().SetColor(new Color(1, 1, 1, 1)); 
             this.Target = null;
-
+        }
     }
 
     void OnDrawGizmos()
@@ -50,8 +56,7 @@ public class PickObject : MonoBehaviour
     /// </summary>
     private void ProcessMouseState()
     {
-
-
+        // 狀態:點擊 滑鼠按下時間>門檻時 改變狀態為拖曳
         if (mouseType == MouseType.點擊)
         {
             clickTime += Time.deltaTime;
@@ -61,12 +66,25 @@ public class PickObject : MonoBehaviour
                 if (Target) print(Target.transform.name + " 已被選取");
             }
         }
-
+        //狀態：拖曳　改變XY座標　當滑鼠放開時　狀態：放開
         if (mouseType == MouseType.拖曳中)
         {
             if (Target)
+            {
                 Target.transform.position = new Vector3(this.camera.ScreenToWorldPoint(Input.mousePosition).x,
                     this.camera.ScreenToWorldPoint(Input.mousePosition).y, Target.transform.position.z);
+
+                if (!this.GetComponent<iTween>())
+                    iTween.ValueTo(this.gameObject, iTween.Hash(
+                        "name", "PickObject",
+                        "from", 1,
+                        "to", 0.25F,
+                        "onupdate", "changePictureAlpha",
+                        "loopType", "pingPong",
+                        "time", 1
+                        ));
+            }
+
 
             if (!Input.GetKey(KeyCode.Mouse0))
             {
@@ -74,9 +92,13 @@ public class PickObject : MonoBehaviour
             }
         }
 
+        //狀態：放開　當滑鼠點擊時　狀態：點擊
         if (mouseType == MouseType.放開)
         {
             this.Target = this.hit.transform.gameObject;
+
+            iTween.StopByName("PickObject");
+            Target.GetComponent<SmoothMoves.Sprite>().SetColor(new Color(1, 1, 1, 1));
             clickTime = 0;
 
             if (Input.GetKey(KeyCode.Mouse0))
@@ -89,7 +111,6 @@ public class PickObject : MonoBehaviour
 
     void changePictureAlpha(float newValue)
     {
-        print(newValue);
         if (Target)
             Target.GetComponent<SmoothMoves.Sprite>().SetColor(new Color(1, 1, 1, newValue));
     }
