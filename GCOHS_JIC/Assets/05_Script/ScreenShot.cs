5b327bfa460191f76fc0fddacf49866f9cb970b8
Ian-Huang <dk781020@hotmail.com>
2013-09-09 21:23:43 +0800

    Update 圖檔

---
+++ GCOHS_JIC/Assets/05_Script/ScreenShot.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenShot : MonoBehaviour
+{
+    public static ScreenShot script;
+
+    private string imagePath;
+
+    // Use this for initialization
+    void Awake()
+    {
+        script = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            this.RunScreenCapture();
+        }
+    }
+
+    public void RunScreenCapture()
+    {
+        Application.CaptureScreenshot("ScreenCapture.png");
+        if (Application.platform == RuntimePlatform.Android || Application.platform == RuntimePlatform.IPhonePlayer)
+            this.imagePath = Application.persistentDataPath;
+        else if (Application.platform == RuntimePlatform.WindowsPlayer)
+            this.imagePath = Application.dataPath;
+        else if (Application.platform == RuntimePlatform.WindowsEditor)
+        {
+            this.imagePath = Application.dataPath;
+            this.imagePath = imagePath.Replace("/Assets", null);
+        }
+        this.imagePath = this.imagePath + "/ScreenCapture.png";
+        print(this.imagePath);
+    }
+}
