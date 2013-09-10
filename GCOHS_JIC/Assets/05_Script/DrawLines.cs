f462c786cf7fa4a7ea67485d4e6730c90593396d
Ian-Huang <dk781020@hotmail.com>
2013-09-10 18:19:06 +0800

    畫線功能完成

---
+++ GCOHS_JIC/Assets/05_Script/DrawLines.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DrawLines : MonoBehaviour
+{
+    public Material LineMaterial;
+    public float LineSize;
+
+    private Vector3 currentPoint;
+    private Vector3 lastPoint = new Vector3(0, 0, -100.0f);
+
+    private int currentLineIndex = 0;
+
+    void Update()
+    {
+        //測試用，清空所有畫線
+        if (Input.GetKeyDown(KeyCode.C))
+            foreach (var line in this.transform.GetComponentsInChildren<LineRenderer>())
+            {
+                Destroy(line.gameObject);
+                this.currentLineIndex = 0;
+            }
+
+
+        if (Input.GetMouseButton(0))
+        {
+            this.currentPoint = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -Camera.main.transform.position.z));
+
+            if (this.lastPoint.z != -100.0f)
+                this.CreateLine(this.lastPoint, this.currentPoint, this.LineSize);
+
+            this.lastPoint = this.currentPoint;
+        }
+        else
+            this.lastPoint.z = -100.0f;
+    }
+
+    void CreateLine(Vector3 start, Vector3 end, float lineSize)
+    {
+        GameObject newLine = new GameObject("Line" + currentLineIndex);
+        newLine.transform.parent = transform;
+
+        LineRenderer lineRender = (LineRenderer)newLine.AddComponent<LineRenderer>();
+        lineRender.material = this.LineMaterial;
+        lineRender.useWorldSpace = false;
+        lineRender.SetWidth(lineSize, lineSize);    //線條粗細
+        lineRender.SetVertexCount(2);               //設定兩個頂點
+        lineRender.SetPosition(0, start);           //線條起始點
+        lineRender.SetPosition(1, end);             //線條終點
+
+        this.currentLineIndex++;    //編號
+    }
+}
+
+
+
+
+
+
+
