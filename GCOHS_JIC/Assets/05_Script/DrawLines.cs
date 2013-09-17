22a1563b99ce5ab740956186a2bf04010f08a2c8
Ian-Huang <dk781020@hotmail.com>
2013-09-17 22:07:58 +0800

    Update draw line

--- GCOHS_JIC/Assets/05_Script/DrawLines.cs
+++ GCOHS_JIC/Assets/05_Script/DrawLines.cs
@@ -7,6 +7,8 @@ public class DrawLines : MonoBehaviour
     public Material LineMaterial;
     public float LineSize;
 
+    public GameManager.RectArea 簽名範圍;
+
     private Vector3 currentPoint;
     private Vector3 lastPoint = new Vector3(0, 0, -100.0f);
 
@@ -28,7 +30,10 @@ public class DrawLines : MonoBehaviour
             this.currentPoint = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -Camera.main.transform.position.z));
 
             if (this.lastPoint.z != -100.0f)
-                this.CreateLine(this.lastPoint, this.currentPoint, this.LineSize);
+                if (this.簽名範圍.isContainArea(this.currentPoint))
+                {
+                    this.CreateLine(this.lastPoint, this.currentPoint, this.LineSize);
+                }
 
             this.lastPoint = this.currentPoint;
         }
@@ -50,6 +55,24 @@ public class DrawLines : MonoBehaviour
         lineRender.SetPosition(1, end);             //線條終點
 
         this.currentLineIndex++;    //編號
+    }
+
+    void OnDrawGizmos()
+    {
+        if (this.簽名範圍.左上 != null && this.簽名範圍.右下 != null)
+        {
+            //顯示左上、右下兩點
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawSphere(this.簽名範圍.左上.position, 10);
+            Gizmos.DrawSphere(this.簽名範圍.右下.position, 10);
+
+            //劃出邊界
+            Gizmos.color = Color.red;
+            Gizmos.DrawRay(this.簽名範圍.左上.position, Vector3.right * Mathf.Abs(this.簽名範圍.左上.position.x - this.簽名範圍.右下.position.x));
+            Gizmos.DrawRay(this.簽名範圍.右下.position, Vector3.left * Mathf.Abs(this.簽名範圍.左上.position.x - this.簽名範圍.右下.position.x));
+            Gizmos.DrawRay(this.簽名範圍.左上.position, Vector3.down * Mathf.Abs(this.簽名範圍.左上.position.y - this.簽名範圍.右下.position.y));
+            Gizmos.DrawRay(this.簽名範圍.右下.position, Vector3.up * Mathf.Abs(this.簽名範圍.左上.position.y - this.簽名範圍.右下.position.y));
+        }
     }
 }
 
