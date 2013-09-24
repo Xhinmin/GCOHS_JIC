db59c9be148a0d7e12d4bb828937538996e46b32
Ian-Huang <dk781020@hotmail.com>
2013-09-24 16:12:21 +0800

    Update ShowDate.cs

--- GCOHS_JIC/Assets/05_Script/ShowDate.cs
+++ GCOHS_JIC/Assets/05_Script/ShowDate.cs
@@ -3,17 +3,27 @@ using System.Collections;
 
 public class ShowDate : MonoBehaviour
 {
+    public Rect TextRect;
+    public GUIStyle style;
+    private string DateString;
+    private float originFontSize;
     private System.DateTime time = System.DateTime.Now;
 
     // Use this for initialization
     void Start()
     {
-
+        this.originFontSize = this.style.fontSize;
     }
 
     // Update is called once per frame
     void Update()
     {
-        print(time.Year.ToString() + @"/" + time.Month.ToString() + @"/" + time.Day);
+        this.DateString = time.Year.ToString() + @"/" + time.Month.ToString() + @"/" + time.Day;
+        this.style.fontSize = (int)(this.originFontSize * (float)(Screen.width / 1280.0f));
+    }
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(this.TextRect.x * Screen.width, this.TextRect.y * Screen.height, this.TextRect.width * Screen.width, this.TextRect.height * Screen.height), this.DateString, this.style);
     }
 }
