7628557e8ea267d9c12b2db315ab201ef55e2c3a
Ian-Huang <dk781020@hotmail.com>
2013-09-24 13:19:27 +0800

    Update

---
+++ GCOHS_JIC/Assets/05_Script/ShowDate.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowDate : MonoBehaviour
+{
+    private System.DateTime time = System.DateTime.Now;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(time.Year.ToString() + @"/" + time.Month.ToString() + @"/" + time.Day);
+    }
+}
